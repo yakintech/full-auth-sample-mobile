6fe5313fa6fe1b3f783f325d63a82efe78db8749
cagatayyildiz <yildiz.cagatay@hotmail.com>
2023-06-22 10:20:51 +0300

    auth sample

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Button, SafeAreaView } from 'react-native'
+import React, { useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { Provider } from 'react-redux'
 import Main from './Main'
@@ -9,13 +9,77 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 const App = () => {
 
   return (<>
-    <NavigationContainer>
+    <UploadSampe />
+    {/* <NavigationContainer>
       <Provider store={store}>
         <Main/>
       </Provider>
-    </NavigationContainer>
+    </NavigationContainer> */}
   </>
   )
 }
 
 export default App
+
+
+
+import { launchImageLibrary } from 'react-native-image-picker';
+import axios from 'axios'
+
+const UploadSampe = () => {
+
+  const [file, setfile] = useState<any>({})
+
+  let options = {
+    title: 'Select Image',
+    customButtons: [
+      {
+        name: 'customOptionKey',
+        title: 'Choose Photo from Custom Option'
+      },
+    ],
+    storageOptions: {
+      skipBackup: true,
+      path: 'images',
+    },
+  };
+
+
+  const upload = () => {
+    launchImageLibrary({
+      mediaType: 'photo'
+    }, setfile)
+
+  }
+
+
+  const postFile = () => {
+    
+    const formData = new FormData();
+    // formData.append("profileImg", file.assets[0].uri);
+    formData.append("profileImg", {
+      uri: file.assets[0].uri,
+      name: file.assets[0].fileName,
+      type: file.assets[0].type,
+    });
+
+    formData.append('userId',3)
+
+    axios.post('http://localhost:3000/api/upload', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+
+  }
+
+  return (<SafeAreaView>
+    <Button title='upload' onPress={upload}></Button>
+    <Button title='POST' onPress={postFile}></Button>
+
+  </SafeAreaView>)
+
+}
+
+
+
